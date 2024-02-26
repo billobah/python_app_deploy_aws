855e332b16b772ea47f2a06ae76a41c981f1f38a
Boubacar Billo Bah <billobah@yahoo.fr>
2024-02-26 18:55:19 +0100

    adding app variable

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
 application = Flask(__name__) # application is a new instance of Flask
 
-@application.route('/') # create a simple endpoint - default web page
+app = application
+
+@app.route('/') # create a simple endpoint - default web page
 def hello_world():
     return 'Hello there good sir.'
